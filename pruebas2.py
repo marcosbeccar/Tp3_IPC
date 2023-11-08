1af61091936728779b925ca4dc2b9c01a59d0a6e
marcosbeccar <beccarvarelam@udesa.edu.ar>
2023-11-08 20:36:39 -0300

    kajsdn

--- pruebas2.py
+++ pruebas2.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from datetime import datetime as dt
+
 
 #--provisional--#
 file_simple_M=r'C:\Users\narco\OneDrive\VS Code\Tp3_IPC\transacciones_simple.txt'
@@ -64,32 +66,73 @@ with open(file_simple_M,'r') as file:
                 if nombre not in deudas:
                     deudas[nombre]=0
             if pagador != '*':
-                lista_fechas.append(fecha)
-            if pagador != '*':
-                deudas[pagador]-=round(monto)
-                deuda=monto/len(deudores)
-                for persona in deudores:
-                    deudas[persona]+=round(deuda)
-                deudas_iteracion = deudas.copy()  # Agregar una copia independiente
-                historial_deudas.append(deudas_iteracion)  # Guardar esta copia en la lista
+                if fecha not in lista_fechas: 
+                    lista_fechas.append(fecha)
+                    deudas[pagador]-=round(monto)
+                    deuda=monto/len(deudores)
+                    for persona in deudores:
+                        deudas[persona]+=round(deuda)
+                    deudas_iteracion = deudas.copy()  # Agregar una copia independiente
+                    historial_deudas.append(deudas_iteracion)  # Guarda esta copia en la lista
+                else:
+                    deudas[pagador]-=round(monto)
+                    deuda=monto/len(deudores)
+                    for persona in deudores:
+                        deudas[persona]+=round(deuda)
+                    
         return deudas, lista_fechas, historial_deudas
     
     
     def grafico_evolucion(historial_deudas, lista_fechas, inquilinos):
         for nombre in inquilinos:
-            deuda_por_persona = [deuda.get(nombre, 0) for deuda in historial_deudas]
+            deuda_por_persona = []
+            for deuda in historial_deudas:
+                deuda_por_persona.append(deuda.get(nombre)) #busca el nombre en cada diccionario de deudas
             plt.plot(lista_fechas, deuda_por_persona, label=nombre)
 
         plt.xlabel('Fechas')
         plt.ylabel('Deuda')
         plt.title('Evolución de las deudas por persona')
         plt.legend()
-        plt.xticks(rotation=45)
+        plt.xticks(rotation=60)
         plt.tight_layout()
-        plt.show()
+        #plt.show()
         
+    def grafico_torta(deudas,fecha_usuario):
+        fechas = [] 
+        for fecha in lista_fechas:  
+            fecha_convertida = dt.strptime(fecha, '%Y-%m-%d')  # Convertir cada fecha a un objeto datetime
+            fechas.append(fecha_convertida)
+        primera_fecha = min(fechas)
+        ultima_fecha = max(fechas)  
+        fecha_usuario_dt= dt.strptime(fecha_usuario, '%Y-%m-%d')
+        
+        if primera_fecha <= fecha_usuario_dt <= ultima_fecha:
+            if fecha_usuario in lista_fechas:
+                indice=lista_fechas.index(fecha_usuario)
+                datos_buscados=historial_deudas[indice]
+            else:
+                ultima_fecha_registrada = None
+                for fecha in lista_fechas:
+                    if fecha < fecha_usuario_dt:
+                        ultima_fecha_registrada = fecha
+                    else:
+                        break
+                indice=lista_fechas.index(ultima_fecha_registrada)
+                datos_buscados=historial_deudas[indice]
+        else:
+            print("--Fecha inválida--")
+        for nombre in inquilinos:
+            datos1=[]
+            datos1.append(deudas.get(nombre))
+        print(datos1)
+            
+        
+        
+    fecha_usuario= '2022-02-01'   
     deudas, lista_fechas, historial_deudas=calculo_deuda()
     grafico_evolucion(historial_deudas, lista_fechas, inquilinos)
+    grafico_torta(deudas,fecha_usuario)
             
             
 
